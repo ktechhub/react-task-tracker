d69f4393b0ad4616bcc0eef6e222225e44cdaacd
Mumuni Mohammed <mumunim10@gmail.com>
2021-07-16 09:44:07 +0000

    Set inline css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,22 @@ import PropTypes from 'prop-types'
 const Header = ({ title }) => {
     return (
         <header>
-            <h1>{title}</h1>
+            <h1 style={{ color: 'blue', backgroundColor: 'black' }}>{title}</h1>
         </header>
     )
 }
 
-// Header.defaultProps = {
-//     title: 'Task Tracker',
-// }
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
 
 Header.propTypes = {
     title: PropTypes.string.isRequired,
 
+}
+
+const headingStyle = {
+    
 }
 
 export default Header
