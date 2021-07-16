11df06244ade8b4783005ca72c805d43fcfcd108
Mumuni Mohammed <mumunim10@gmail.com>
2021-07-16 11:11:45 +0000

    Created the toggle reminder listener

--- src/App.js
+++ src/App.js
@@ -9,19 +9,19 @@ function App() {
                 id: 1,
                 text: 'Task 1',
                 day: 'Jan 3rd at 2:00pm',
-                reminder: 'true'
+                reminder: true
             },
             {
                 id: 2,
                 text: 'Task 2',
                 day: 'Jan 23rd at 2:00pm',
-                reminder: 'false'
+                reminder: false
             },
             {
                 id: 3,
                 text: 'Task 3',
                 day: 'Aug 3rd at 2:00pm',
-                reminder: 'true'
+                reminder: true
             }
         ]
     )
