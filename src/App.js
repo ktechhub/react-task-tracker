809357c7e5a88f66bf3a437595f50bc25d86e554
Mumuni Mohammed <mumunim10@gmail.com>
2021-07-16 10:24:48 +0000

    started with tasks page, put the data in a global state for other components to use it.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useState } from "react"
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 function App() {
+
+    const [tasks, setTasks] = useState(
+        [
+            {
+                id: 1,
+                text: 'Task 1',
+                day: 'Jan 3rd at 2:00pm',
+                reminder: 'true'
+            },
+            {
+                id: 2,
+                text: 'Task 2',
+                day: 'Jan 23rd at 2:00pm',
+                reminder: 'false'
+            },
+            {
+                id: 3,
+                text: 'Task 3',
+                day: 'Aug 3rd at 2:00pm',
+                reminder: 'true'
+            }
+        ]
+    )
+
     return ( 
         <div className="container">
             <Header/>
-            <Tasks />
+            <Tasks tasks={tasks}/>
         </div>
     );
 }
