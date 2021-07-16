3fd808ce40edb9ab1adcbf39519d8e66e2dc07b2
Mumuni Mohammed <mumunim10@gmail.com>
2021-07-16 11:09:38 +0000

    Created the toggle reminder listener

--- src/App.js
+++ src/App.js
@@ -27,15 +27,21 @@ function App() {
     )
 
     // Delete Tasks
-
     const deleteTask = (id) => {
-        console.log('Delete', id)
+        setTasks(tasks.filter((task) => task.id !== id))
+        // console.log('Delete', id)
+    }
+
+    // Toggle Reminder
+    const toggleReminder = (id) => {
+        setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+        // console.log('Reminder', id)
     }
 
     return ( 
         <div className="container">
             <Header/>
-            <Tasks tasks={tasks} onDelete={deleteTask}/>
+            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Task'}
         </div>
     );
 }
